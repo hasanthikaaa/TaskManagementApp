fb39900e4b57ccdcaa4d38d23aa750428c8555cb
hasanthikaaa <hasanthika@axis-tech.co>
2025-03-16 15:17:08 +0530

    nodemon setup

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,11 +1,26 @@
-import { typescript } from 'projen';
+import { JsonFile, typescript } from "projen";
+
 const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: 'main',
-  name: 'TaskManagementApp',
+  defaultReleaseBranch: "main",
+  name: "TaskManagementApp",
   projenrcTs: true,
-  deps: ["express"],
-  description:"Backend solution for TaskManagementApp",
+  deps: ["express", "nodemon", "prettier"],
+  description: "Backend solution for TaskManagementApp",
   devDeps: ["@types/node"],
-  packageName: "TaskManagementApp"
+  packageName: "TaskManagementApp",
 });
+
+project.addTask("nodemon", {
+  exec: "nodemon --watch src src/index.ts",
+});
+
+new JsonFile(project, "nodemon.json", {
+  obj: {
+    watch: ["src"],
+    ext: "ts,json",
+    ignore: ["src/**/*.spec.ts"],
+    exec: "ts-node ./src/index.ts",
+  },
+});
+
 project.synth();
